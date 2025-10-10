7bf064684c93f4c3977a5527ef81428c7b075c7a
burrito <toletibharat@gmail.com>
2025-10-10 16:53:08 +0530

    added ack in task 1

--- Assignment 5/stop_and_wait.py
+++ Assignment 5/stop_and_wait.py
@@ -1,17 +1,28 @@
+import time
+
 def receiver(frame_data):
-    """A simple receiver that just accepts and prints the frame it gets."""
+    """A receiver that accepts and acknowledges the frame it gets."""
     print(f"    RECEIVER: Received Frame {frame_data}")
+    ack_number=frame_data
+    print(f"    RECEIVER: Sending ACK {ack_number}")
+    return ack_number
 
 def sender():
-    """A simple sender that sends a sequence of frames."""
+    """A sender that waits for ACK and sends the next frame."""
     total_frames = 5
+    next_frame_to_send = 0
     print("SENDER: Starting to send frames...")
 
-    for i in range(total_frames):
-        print(f"SENDER: Sending Frame {i}")
-        receiver(i)
-        print(f"SENDER: Frame {i} acknowledged\n")
-    
+    while next_frame_to_send < total_frames:
+        print(f"SENDER: Sending Frame {next_frame_to_send}...")
+        received_ack = receiver(next_frame_to_send)
+        print(f"SENDER: Received ACK {received_ack}")
+        if(received_ack == next_frame_to_send):
+            print(f"SENDER: ACK {received_ack} is correct. Moving to next frame.")
+            next_frame_to_send += 1
+        else:
+            print(f"SENDER: Incorrect ACK. This shouldn't happen yet.\n")
+        time.sleep(1)
     print("SENDER: All frames sent and acknowledged.")
 
 if __name__ == "__main__":
